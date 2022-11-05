63164136a3b96d2f7bf9ce4a38a91e002554b1e1
Claudio Ferreira <claudio.ferreira@aizoon.it>
2022-11-05 15:59:26 +0100

    first-commit

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,34 @@
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons'
+import Logo from '../assets/logo.svg'
+import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
+
+export function SignIn() {
+
+  const { signIn, isUserLoading } = useAuth();
+
+
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        title="Entrar com o Google"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto}
+          name="google"
+          color="white"
+          size="md"
+        />}
+        mt={12}
+        onPress={signIn}
+        isLoading={isUserLoading}
+        _loading={{ _spinner: { color: 'white' } }}
+      />
+
+      <Text color="gray.200" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
+      </Text>
+    </Center>
+  )
+}
